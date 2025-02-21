f263c586eb7f96c8622967fece1ea1180fcb1914
evan baron <egbaron@gmail.com>
2025-02-21 09:56:39 -0700

    cannot navigate to login or register if logged in

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,15 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import '../style.scss';
-import Navbar from '../components/Navbar';
+import axiosInstance from '../utils/axios';
 
 // Pages
 import Home from '../pages/Home';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 
+// Components
+import Navbar from '../components/Navbar';
+
 const App = () => {
 	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUserData = async () => {
+			const token = localStorage.getItem('token')
+
+			if (token) {
+				console.log('Token exists!');
+				try {
+					const response = await axiosInstance.get('/authenticate', {
+						headers: { Authorization: `Bearer ${token}` },
+						withCredentials: true,
+					})
+
+					setUser(response.data);
+				} catch (error) {
+					console.error('Error authenticating: ', error);
+					console.log('No tokens found. Please log in.');
+				}
+			} else if (!token) {
+				console.log('No token found. Checking cookies...');
+				try { 
+					const response = await axiosInstance.get('/authenticate', {
+						withCredneitials: true,
+					});
+
+					setUser(response.data);
+				} catch (error) {
+					console.error('Error authenticating: ', error);
+					console.log('No tokens found. Please log in.');
+				}
+			} else {
+				console.log('No tokens found. Please log in.');
+			}
+
+			setLoading(false);
+		}
+
+		fetchUserData();
+	}, []);
 
 	useEffect(() => {
 		console.log('useEffect triggered!');
@@ -21,9 +64,9 @@ const App = () => {
 				<BrowserRouter>
 					<Navbar user={user} setUser={setUser} />
 					<Routes>
-						<Route path='/' element={<Home user={user} setUser={setUser} />} />
-						<Route path='/register' element={<Register />} />
-						<Route path='/login' element={<Login user={user} setUser={setUser} />} />
+						<Route path='/' element={<Home loading={loading} user={user} setUser={setUser} />} />
+						<Route path='/register' element={user ? <Navigate to='/' /> : <Register />} />
+						<Route path='/login' element={user ? <Navigate to='/' /> : <Login user={user} setUser={setUser} />} />
 					</Routes>
 				</BrowserRouter>
 			</div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,65 +1,27 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axiosInstance from '../utils/axios';
 
-const Home = ({ user, setUser }) => {
-
-	useEffect(() => {
-		const fetchUserData = async () => {
-			const token = localStorage.getItem('token')
-
-			if (token) {
-				console.log('Token exists!');
-				console.log(token);
-				try {
-					const response = await axiosInstance.get('/authenticate', {
-						headers: { Authorization: `Bearer ${token}` },
-						withCredentials: true,
-					})
-
-					setUser(response.data);
-				} catch (error) {
-					console.error('Error authenticating: ', error);
-				}
-			} else if (!token) {
-				console.log('No token found. Checking cookies...');
-				try { 
-					const response = await axiosInstance.get('/authenticate', {
-						withCredneitials: true,
-					});
-
-					setUser(response.data);
-				} catch (error) {
-					console.error('Error authenticating: ', error);
-				}
-			} else {
-				console.log('No tokens found. Please log in.');
-			}
-		}
-
-		fetchUserData();
-	}, []);
-
-
+const Home = ({ loading, user, setUser }) => {
 	return (
 		<div className="home">
 			<div className="home-container">
-				{user ? (
+				{!loading && (user ? (
 					<h1>{`Welcome, ${user.user.first_name}!`}</h1>
-				) : (
-					<>
-						<h2>Welcome!</h2>
-						<div className="button-container">
-							<Link to='/login' className='button'>
-								Login
-							</Link>
-							<p>or</p>
-							<Link to='/register' className='button'>
-								Register
-							</Link>
-						</div>
-					</>
-				)}
+					) : (
+						<>
+							<h2>Welcome!</h2>
+							<div className="button-container">
+								<Link to='/login' className='button'>
+									Login
+								</Link>
+								<p>or</p>
+								<Link to='/register' className='button'>
+									Register
+								</Link>
+							</div>
+						</>
+					))
+				}
 			</div>
 		</div>
 	);
