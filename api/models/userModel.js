1989a8cd4cecf65fc4b8388c2ec2c0db7380ca50
evan-baron <egbaron@gmail.com>
2025-03-05 13:03:14 -0700

    deleted unnecessary route

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -46,12 +46,6 @@ const User = {
 		return rows.length > 0; // Return true if email exists, otherwise false
 	},
 
-	// Delete a user by ID
-	async deleteUserById(id) {
-		const [result] = await pool.execute('DELETE FROM users WHERE id = ?', [id]);
-		return result.affectedRows > 0;
-	},
-
 	// Update user password
 	async updateUserPassword(hashedPassword, token) {
 		const [result] = await pool.execute('UPDATE users SET password = ? WHERE email = (SELECT user_email FROM email_recovery_tokens WHERE token = ?)', [hashedPassword, token]);
